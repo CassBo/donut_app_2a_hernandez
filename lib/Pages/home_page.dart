c0dbfb9f6700fccf074f7b134797fcc97d176687
CassBo <karmeterskate@gmail.com>
2025-02-14 09:03:30 -0600

    Starting TabBarView

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,3 +1,9 @@
+import 'package:donut_app_2a_hernandez/Utils/my_tab.dart';
+import 'package:donut_app_2a_hernandez/tabs/burger_tab.dart';
+import 'package:donut_app_2a_hernandez/tabs/donut_tab.dart';
+import 'package:donut_app_2a_hernandez/tabs/pancakes_tab.dart';
+import 'package:donut_app_2a_hernandez/tabs/pizza_tab.dart';
+import 'package:donut_app_2a_hernandez/tabs/smoothie_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,50 +14,79 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+List<Widget> myTabs = [
+//Donut Tab
+MyTab(iconPath: 'lib/icons/donut.png'),
+//Burger Tab
+MyTab(iconPath: 'lib/icons/burger.png'),
+//Smoothie Tab
+MyTab(iconPath: 'lib/icons/smoothie.png'),
+//Pancake Tab
+MyTab(iconPath: 'lib/icons/pancakes.png'),
+//Pizza Tab
+MyTab(iconPath: 'lib/icons/pizza.png'),
+];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        // Icono de la izquierda
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey[800],
+    //Este widget sirve para gestionar pestañas
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          // Icono de la izquierda
+          leading: Icon(
+            Icons.menu,
+            color: Colors.grey[800],
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Icon(Icons.person),
+            )
+          ],
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Icon(Icons.person),
-          )
-        ],
-      ),
-      body: const Column( // Agregado: 'const' está bien aquí
-        children: [
-          // Texto
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Text(
-                  "I want to",
-                  style: TextStyle(fontSize: 32),
-                ),
-                Text(
-                  "Eat",
-                  style: TextStyle(
-                    // Tamaño de la letra
-                    fontSize: 32,
-                    // Negritas
-                    fontWeight: FontWeight.bold,
-                    // Subrayado
-                    decoration: TextDecoration.underline,
+        body:  Column( // Agregado: 'const' está bien aquí
+          children: [
+            // Texto
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text(
+                    "I want to ",
+                    style: TextStyle(fontSize: 32),
                   ),
-                ),
-              ], // Agregado: Cierre de la lista de hijos del Row
-            ), // Agregado: Cierre del Row
-          ), // Agregado: Cierre del Padding
-        ], // Agregado: Cierre de la lista de hijos del Column
-      ), // Agregado: Cierre del Column
+                  Text(
+                    "Eat",
+                    style: TextStyle(
+                      // Tamaño de la letra
+                      fontSize: 32,
+                      // Negritas
+                      fontWeight: FontWeight.bold,
+                      // Subrayado
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            TabBar(tabs: myTabs),
+            //Tab bar view
+            Expanded(
+              child: TabBarView(children:[
+                DonutTab(),
+                BurgerTab(),
+                SmoothieTab(),
+                PanCakesTab(),
+                PizzaTab(),
+              ]) ,
+            ),
+          ],
+        ), // Agregado: Cierre del Column
+      ),
     ); // Agregado: Cierre del Scaffold
   }
 }
